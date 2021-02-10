450f36bb40fc27b4a0340d683d7aa2c92681e45c
DenApache <DenApache@DESKTOP-TPO6P62.home>
2021-02-10 16:22:05 +0100

    Prise en compte de l'ip du serveur

--- src/client/Client.java
+++ src/client/Client.java
@@ -13,6 +13,9 @@ import serveur.GameManager;
 
 public class Client {
 	
+	public static final String SERVEUR_IP = "192.168.1.16";
+	public static final String URL = "rmi://"+ SERVEUR_IP;
+	
 	 public static int getInput(){
         System.out.println("1 - Create \n2 - Join");
         
@@ -25,27 +28,35 @@ public class Client {
 
         @SuppressWarnings("unused")
 		Registry registry = LocateRegistry.getRegistry(1099);
-        GameCreator stubMaster = (GameCreator) Naming.lookup("Creator");
+        GameCreator stubMaster = (GameCreator) Naming.lookup(URL + "/Creator");
         String gameName = null;
 
+        // Creation d'une partie
         if(Client.getInput() == 1) {
             gameName = stubMaster.createGame();
             System.out.println("Game :" + gameName);
 
-            GameManager stubSlave = (GameManager) Naming.lookup(gameName);
-            stubSlave.joinGame("Player1");
-        } else {
+            GameManager stubSlave = (GameManager) Naming.lookup(URL + gameName);//IP
+            stubSlave.joinGame("Player1");  
+        }
+        // Connection a une partie
+        else {
             gameName = stubMaster.findGame();
             if(gameName == null)
                 System.out.println("Aucune partie trouvee");
             else {
                 System.out.println("Partie trouvee : " + gameName);
-                GameManager stubSlave = (GameManager) Naming.lookup(gameName);
+                GameManager stubSlave = (GameManager) Naming.lookup(URL + gameName);//IP
 
                 stubSlave.joinGame("Player2");
             }
         }
 
         while(true);
+        // Attaquer
+        // Déplacer
+        // Passer son tour
+        
+        
     }
 }
